6e1aad9ebedee482a2224e086bfd8b379feb4b4b
GeumBi-Hong <canbe159@gmail.com>
2022-11-23 00:15:34 +0900

    feat:아파트 실거래가 차트 구현

--- frontend/src/components/apart/ApartDetail.vue
+++ frontend/src/components/apart/ApartDetail.vue
@@ -6,15 +6,22 @@
           <h1 style="font-size: 20px">
             <span>{{ this.$route.params.apartCode }}</span>
           </h1>
-          <a href="#" class="btn-back" data-ga-event="apt,backBtn"><span> 뒤로 </span></a
-          ><a href="#" class="btn-close" data-ga-event="apt,closeBtn"><span>X</span></a>
+          <a href="#" class="btn-back" data-ga-event="apt,backBtn"
+            ><span> 뒤로 </span></a
+          ><a href="#" class="btn-close" data-ga-event="apt,closeBtn"
+            ><span>X</span></a
+          >
         </div>
 
         <div class="address-info">
-          <h2 class="address" style="font-size: 10px">지번 :경기도 부천시 괴안동 162-2</h2>
+          <h2 class="address" style="font-size: 10px">
+            지번 :경기도 부천시 괴안동 162-2
+          </h2>
         </div>
         <div class="address-info">
-          <h2 class="address" style="font-size: 10px">도로명 :경기도 부천시 괴안동 162-2</h2>
+          <h2 class="address" style="font-size: 10px">
+            도로명 :경기도 부천시 괴안동 162-2
+          </h2>
         </div>
 
         <ul class="search-select-group">
@@ -28,12 +35,28 @@
             </div>
           </li>
           <li class="area select">
-            <button type="button" class="areaSelector" data-ga-event="select,area">29평</button>
+            <button
+              type="button"
+              class="areaSelector"
+              data-ga-event="select,area"
+            >
+              29평
+            </button>
           </li>
-          <li class="comment"><a href="/apt/5wW15/0/2/review" data-ga-event="apt,viewReviewOnHeader">38</a></li>
+          <li class="comment">
+            <a
+              href="/apt/5wW15/0/2/review"
+              data-ga-event="apt,viewReviewOnHeader"
+              >38</a
+            >
+          </li>
           <li class="options">
             <div>
-              <b-form-select v-model="apartArea" :options="areas"></b-form-select>
+              <b-form-select
+                v-model="apartArea"
+                :options="areas"
+                @change="changeArea"
+              ></b-form-select>
             </div>
           </li>
         </ul>
@@ -51,6 +74,35 @@
           :width="width"
           :height="height"
         />
+      </div>
+
+      <div class="map_wrap">
+        <ul id="category">
+          <li id="BK9" data-order="0">
+            <span class="category_bg bank"></span>
+            은행
+          </li>
+          <li id="MT1" data-order="1">
+            <span class="category_bg mart"></span>
+            마트
+          </li>
+          <li id="PM9" data-order="2">
+            <span class="category_bg pharmacy"></span>
+            약국
+          </li>
+          <li id="OL7" data-order="3">
+            <span class="category_bg oil"></span>
+            주유소
+          </li>
+          <li id="CE7" data-order="4">
+            <span class="category_bg cafe"></span>
+            카페
+          </li>
+          <li id="CS2" data-order="5">
+            <span class="category_bg store"></span>
+            편의점
+          </li>
+        </ul>
       </div>
     </div>
     <div id="map"></div>
@@ -109,7 +161,9 @@ export default {
         datasets: [
           {
             label: "Data One",
-            backgroundColor: "#f87979",
+            backgroundColor: "#2e2929",
+            borderColor: "#1A9A91",
+
             data: [],
           },
         ],
@@ -117,6 +171,45 @@ export default {
       chartOptions: {
         responsive: true,
         maintainAspectRatio: false,
+
+        scales: {
+          y: {
+            grid: {
+              display: false,
+            },
+            ticks: {
+              callback: function (value) {
+                console.log(value + "인덱스");
+                let length = value.toString().length;
+                console.log(value.length);
+                if (length >= 5) {
+                  return (
+                    value.toString().substring(0, length - 4) +
+                    "억 " +
+                    value.toString().substring(length - 4, length) +
+                    "만원"
+                  );
+                } else {
+                  return value + "만원";
+                }
+              },
+            },
+          },
+        },
+
+        // scales: {
+        //   yAxes: [
+        //     {
+        //       ticks: {
+        //         stepSize: 20, // 증가범위
+        //         beginAtZero: true,
+        //         max: 100, // 최대범위
+        //         min: 0, // 최소범위
+        //         padding: 10, // 오른쪽 간격
+        //       },
+        //     },
+        //   ],
+        // },
       },
     };
   },
@@ -162,23 +255,44 @@ export default {
       console.log(this.$route.params.apartCode + " " + "create");
       // this.chartData.labels = null;
 
-      await http.get(`apart/area/${this.$route.params.apartCode}`).then(({ data }) => {
-        data.forEach((area) => {
-          this.areas.push({ value: area, text: area / 3.30579 });
+      await http
+        .get(`apart/area/${this.$route.params.apartCode}`)
+        .then(({ data }) => {
+          data.forEach((area) => {
+            this.areas.push({ value: area, text: Math.round(area / 3.30579) });
+          });
+
+          this.apartArea = this.areas[0].value;
         });
 
-        this.apartArea = this.areas[0];
-        console.log(this.areas[0]);
+      await this.getApartDealWithArea();
+    },
 
-        console.log(this.apartArea.value + "평");
-      });
+    getApartDealWithArea() {
+      http
+        .get(`apart/view/${this.$route.params.apartCode}/${this.apartArea}`)
+        .then(({ data }) => {
+          console.log("아파트거래정보");
+          for (var i = 0; i < data.length; i++) {
+            this.chartData.labels.push(
+              data[i].dealYear + "년 " + data[i].dealMonth + "월"
+            );
 
-      await http.get(`apart/view/${this.$route.params.apartCode}/${this.apartArea.value}`).then(({ data }) => {
-        for (var i = 0; i < data.length; i++) {
-          this.chartData.labels.push(data[i].dealYear + "년 " + data[i].dealMonth + "월");
-          this.chartData.datasets[0].data.push(data[i].amount);
-        }
-      });
+            console.log(data[i].amount);
+            this.chartData.datasets[0].data.push(data[i].amount);
+          }
+        });
+    },
+
+    changeArea() {
+      //리스트 초기화
+      this.initChartData();
+      this.getApartDealWithArea();
+    },
+
+    initChartData() {
+      this.chartData.labels = [];
+      this.chartData.datasets[0].data = [];
     },
   },
 };
@@ -199,7 +313,7 @@ export default {
   top: 0;
   left: 0;
   width: 430px;
-  background-color: orange;
+  background-color: #1a9a91;
   padding-top: 12px;
   color: #ffffff;
 }
@@ -236,5 +350,147 @@ li {
   display: list-item;
   text-align: -webkit-match-parent;
   list-style: none;
+}
+
+.map_wrap,
+.map_wrap * {
+  margin: 0;
+  padding: 0;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  font-size: 12px;
+}
+.map_wrap {
+  position: relative;
+  width: 100%;
+  height: 350px;
+}
+#category {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  border-radius: 5px;
+  border: 1px solid #909090;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
+  background: #fff;
+  overflow: hidden;
+  z-index: 2;
+}
+#category li {
+  float: left;
+  list-style: none;
+  width: 50px;
+  border-right: 1px solid #acacac;
+  padding: 6px 0;
+  text-align: center;
+  cursor: pointer;
+}
+#category li.on {
+  background: #eee;
+}
+#category li:hover {
+  background: #ffe6e6;
+  border-left: 1px solid #acacac;
+  margin-left: -1px;
+}
+#category li:last-child {
+  margin-right: 0;
+  border-right: 0;
+}
+#category li span {
+  display: block;
+  margin: 0 auto 3px;
+  width: 27px;
+  height: 28px;
+}
+#category li .category_bg {
+  background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png)
+    no-repeat;
+}
+#category li .bank {
+  background-position: -10px 0;
+}
+#category li .mart {
+  background-position: -10px -36px;
+}
+#category li .pharmacy {
+  background-position: -10px -72px;
+}
+#category li .oil {
+  background-position: -10px -108px;
+}
+#category li .cafe {
+  background-position: -10px -144px;
+}
+#category li .store {
+  background-position: -10px -180px;
+}
+#category li.on .category_bg {
+  background-position-x: -46px;
+}
+.placeinfo_wrap {
+  position: absolute;
+  bottom: 28px;
+  left: -150px;
+  width: 300px;
+}
+.placeinfo {
+  position: relative;
+  width: 100%;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  border-bottom: 2px solid #ddd;
+  padding-bottom: 10px;
+  background: #fff;
+}
+.placeinfo:nth-of-type(n) {
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
+.placeinfo_wrap .after {
+  content: "";
+  position: relative;
+  margin-left: -12px;
+  left: 50%;
+  width: 22px;
+  height: 12px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+}
+.placeinfo a,
+.placeinfo a:hover,
+.placeinfo a:active {
+  color: #fff;
+  text-decoration: none;
+}
+.placeinfo a,
+.placeinfo span {
+  display: block;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+.placeinfo span {
+  margin: 5px 5px 0 5px;
+  cursor: default;
+  font-size: 13px;
+}
+.placeinfo .title {
+  font-weight: bold;
+  font-size: 14px;
+  border-radius: 6px 6px 0 0;
+  margin: -1px -1px 0 -1px;
+  padding: 10px;
+  color: #fff;
+  background: #d95050;
+  background: #d95050
+    url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png)
+    no-repeat right 14px center;
+}
+.placeinfo .tel {
+  color: #0f7833;
+}
+.placeinfo .jibun {
+  color: #999;
+  font-size: 11px;
+  margin-top: 0;
 }
 </style>
